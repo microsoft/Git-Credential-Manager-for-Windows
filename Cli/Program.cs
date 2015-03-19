79b45f21bda1ea02272ab2815c5b22e20076f54e
J Wyman <jeremy.wyman@microsoft.com>
2015-03-19 11:09:45 -0700

    added trace listeners to Program

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -18,32 +18,43 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(true);
 
+            EnabelDebugTrace();
+
+            // parse the operations arguments from stdin (this is how git sends commands)
+            // see: https://www.kernel.org/pub/software/scm/git/docs/technical/api-credentials.html
+            // see: https://www.kernel.org/pub/software/scm/git/docs/git-credential.html
             OperationArguments operationArguments = new OperationArguments(Console.In);
 
+            // parse the git config for related values
             using (Repository repo = new Repository(Environment.CurrentDirectory))
             {
                 GitConfigValue match = null;
 
                 if ((match = GetConfig(repo, operationArguments, "authority")) != null)
                 {
+                    Trace.TraceInformation("authority = {0}", match.Value);
                     operationArguments.SetScheme(match.Value);
                 }
                 if ((match = GetConfig(repo, operationArguments, "clientid")) != null)
                 {
+                    Trace.TraceInformation("clientid = {0}", match.Value);
                     operationArguments.AuthorityClientId = match.Value;
                 }
                 if ((match = GetConfig(repo, operationArguments, "resource")) != null)
                 {
+                    Trace.TraceInformation("resource = {0}", match.Value);
                     operationArguments.AuthorityResource = match.Value;
                 }
                 if ((match = GetConfig(repo, operationArguments, "tenantid")) != null)
                 {
+                    Trace.TraceInformation("tenantid = {0}", match.Value);
                     operationArguments.AuthorityTenantId = match.Value;
                 }
             }
 
             foreach (string arg in args)
             {
+                Trace.TraceInformation("git: command: {0}", arg);
                 switch (arg)
                 {
                     case "erase":
@@ -61,8 +72,12 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
 
         private static void Erase(OperationArguments operationArguments)
         {
+            Trace.TraceInformation("Program.Erase");
+
             Debug.Assert(operationArguments != null, "The operationArguments is null");
             Debug.Assert(operationArguments.TargetUri != null, "The operationArgument.TargetUri is null");
+
+            Trace.TraceInformation("targetUri = {0}", operationArguments.TargetUri);
 
             BaseAuthentication authentication = CreateAuthentication(operationArguments);
             authentication.DeleteCredentials(operationArguments.TargetUri);
@@ -70,30 +85,39 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
 
         private static void Get(OperationArguments operationArguments)
         {
+            Trace.TraceInformation("Program.Get");
+
             Debug.Assert(operationArguments != null, "The operationArguments is null");
             Debug.Assert(operationArguments.TargetUri != null, "The operationArgument.TargetUri is null");
+
+            Trace.TraceInformation("targetUri = {0}", operationArguments.TargetUri);
 
             BaseAuthentication authentication = CreateAuthentication(operationArguments);
             Credentials credentials = null;
 
             if (authentication != null && authentication.GetCredentials(operationArguments.TargetUri, out credentials))
             {
+                Trace.TraceInformation("credentials found");
                 operationArguments.SetCredentials(credentials);
             }
             else
             {
+                Trace.TraceInformation("credentials not found");
                 // if the authority supports VSO personal access tokens, the username + password promp git provides will be insufficient
                 // instead of relying on Git then failing, open a modal dialog and request credentials from the user
                 // and use those credentials to logon to the service and generate a personal access token
                 // then return the personal access token to the user
                 if (operationArguments.Authority == AuthorityType.AzureDirectory || operationArguments.Authority == AuthorityType.MicrosoftAccount)
                 {
+                    Trace.TraceInformation("authority is {0}, launching credential dialog", operationArguments.Authority);
                     // ask for credentials
                     var dialog = new CredentialForm(operationArguments.TargetUri);
                     dialog.ShowDialog();
 
                     if (dialog.DialogResult == DialogResult.OK)
                     {
+                        Trace.TraceInformation("dialog = OK");
+                        Trace.TraceInformation("username = {0}, password = {1}", dialog.Username, dialog.Password == null ? String.Empty : "******");
                         credentials = new Credentials(dialog.Username, dialog.Password);
                         Task.Run(async () =>
                         {
@@ -101,6 +125,7 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
                             if (await (authentication as BaseVsoAuthentication).InteractiveLogon(operationArguments.TargetUri, credentials)
                                 && authentication.GetCredentials(operationArguments.TargetUri, out credentials))
                             {
+                                Trace.TraceInformation("credentials captured and stored");
                                 operationArguments.SetCredentials(credentials);
                             }
                         })
@@ -114,6 +139,8 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
 
         private static void Store(OperationArguments operationArguments)
         {
+            Trace.WriteLine("Program.Store");
+
             Debug.Assert(operationArguments != null, "The operationArguments is null");
             Debug.Assert(operationArguments.Username != null, "The operaionArgument.Username is null");
             Debug.Assert(operationArguments.Password != null, "The operaionArgument.Password is null");
@@ -128,6 +155,8 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
         private static BaseAuthentication CreateAuthentication(OperationArguments operationArguments)
         {
             Debug.Assert(operationArguments != null, "The operationArguments is null");
+
+            Trace.TraceInformation("creating authentication for {0}", operationArguments.Authority);
 
             switch (operationArguments.Authority)
             {
@@ -148,10 +177,12 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
                                 if (Guid.TryParse(operationArguments.AuthorityTenantId, out tenantId))
                                 {
                                     // return a custom AAD backed VSO authentication objects
+                                    Trace.TraceInformation("resource = {0}, clientId = {1}, tenantId = {2}", resource, clientId, tenantId);
                                     return new VsoAadAuthentication(tenantId, resource, clientId);
                                 }
                             }
                             // return a common tenant AAD backed VSO authentication object
+                            Trace.TraceInformation("resource = {0}, clientId = {1}", resource, clientId);
                             return new VsoAadAuthentication(resource, clientId);
                         }
                     }
@@ -172,6 +203,7 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
                         if (Guid.TryParse(operationArguments.AuthorityClientId, out clientId))
                         {
                             // return a common tenant MSA backed VSO authentication object
+                            Trace.TraceInformation("resource = {0}, clientId = {1}", resource, clientId);
                             return new VsoMsaAuthentation(resource, clientId);
                         }
                     }
@@ -209,6 +241,13 @@ namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
                                      })
                               .OrderByDescending((GitConfigValue entry) => { return entry.Level; })
                               .FirstOrDefault();
+        }
+
+        [Conditional("DEBUG")]
+        private static void EnabelDebugTrace()
+        {
+            // use the stderr stream for the trace as stdout is used in the cross-process communcations protocol
+            Trace.Listeners.Add(new ConsoleTraceListener(useErrorStream: true));
         }
     }
 }
