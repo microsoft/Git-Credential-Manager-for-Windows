6e1a86bf48b8bd3595aa637c63ec25a66715507d
J Wyman <jeremy.wyman@microsoft.com>
2015-03-24 11:00:28 -0400

    added a thread safe, in-memory credential cache class

---
+++ Core/CredentialCache.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
+{
+    class CredentialCache : BaseCredentialStore, ICredentialStore
+    {
+        internal CredentialCache()
+        {
+            _store = new ConcurrentDictionary<string, Credentials>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        private ConcurrentDictionary<string, Credentials> _store;
+
+        public void DeleteCredentials(Uri targetUri)
+        {
+            BaseCredentialStore.ValidateTargetUri(targetUri);
+            string targetName = this.GetTargetName(targetUri);
+
+            Credentials credentials = null;
+            _store.TryRemove(targetName, out credentials);
+        }
+        public bool ReadCredentials(Uri targetUri, out Credentials credentials)
+        {
+            BaseCredentialStore.ValidateTargetUri(targetUri);
+            string targetName = this.GetTargetName(targetUri);
+
+            return _store.TryGetValue(targetName, out credentials);
+        }
+
+        public void WriteCredentials(Uri targetUri, Credentials credentials)
+        {
+            BaseCredentialStore.ValidateTargetUri(targetUri);
+            string targetName = this.GetTargetName(targetUri);
+
+            _store[targetName] = credentials;
+        }
+
+        protected override string GetTargetName(Uri targetUri)
+        {
+            const string PrimaryNameFormat = "{0}://{1}";
+
+            Debug.Assert(targetUri != null, "The targetUri parameter is null");
+            Debug.Assert(targetUri.IsAbsoluteUri, "The targetUri parameter is not absolute");
+
+            // trim any trailing slashes and/or whitespace for compat with git-credential-winstore
+            string trimmedHostUrl = targetUri.Host
+                                             .TrimEnd('/', '\\')
+                                             .TrimEnd();
+            string targetName = String.Format(PrimaryNameFormat, targetUri.Scheme, trimmedHostUrl);
+            return targetName;
+        }
+    }
+}
