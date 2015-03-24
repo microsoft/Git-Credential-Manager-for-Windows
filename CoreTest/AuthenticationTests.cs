6296666f1c4affb45adb273928ffa6eaac1a8985
J Wyman <jeremy.wyman@microsoft.com>
2015-03-24 01:38:45 -0400

    fixup AuthenitcatinoTest usings

--- CoreTest/AuthenticationTests.cs
+++ CoreTest/AuthenticationTests.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
