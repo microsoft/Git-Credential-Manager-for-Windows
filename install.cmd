ed7abdae8344af60d9bcee45423e3155a6164594
J Wyman <jeremy.wyman@microsoft.com>
2015-03-30 10:42:22 -0700

    fixing up install.cmd

--- install.cmd
+++ install.cmd
@@ -1,25 +1,29 @@
+:: Wanto to update this thing and don't know how?
+:: Check http://ss64.com/nt/syntax.html
 @ECHO OFF
 
-SET extensionName=Git Credential Manager
+
+SET gitExtensionName=Git Credential Helper
 
 :CHECK_PERMISSIONS
-	ECHO Administrative permissions required.
-	ECHO Detecting permissions...
+    ECHO Administrative permissions required. 
+    ECHO Detecting permissions...
 
-	net session >nul 2>&1
-	IF %errorLevel% ==0 (
-		ECHO Running as admin. Good!
-	) ELSE (
-		GOTO NEED_ADMIN_ACCESS
-	)
+    net session >nul 2>&1
+    IF %errorLevel% == 0 (
+        ECHO Running as admin. Good!
+    ) ELSE (
+        GOTO NEED_ADMIN_ACCESS
+    )
 
 ::32-bit OS not supported
-IF NOT EXISTS "%programFiles (x86)%" GOTO :LEGACY_OS
+IF NOT EXIST "%ProgramFiles(x86)%" GOTO :LEGACY_OS
 
-ECHO Hello! I'll install %extensionName%.
+ECHO Hello! I'll install "%gitExtensionName%" so that you can interact with Visual Studio Online.
 SET installPath=%~dp0
-SET extensionInstalled=0
-SET fileName=git-credential-man.exe
+SET helperInstalled=0
+SET remoteFileName=git-credential-man.exe
+
 
 :GIT_TOOLS_FOR_MICROSOFT_ENGINEERS
 
@@ -30,9 +34,8 @@ IF NOT EXIST "%exeInstall%" GOTO :MSYSGIT
 
 ECHO I'm installing "%gitExtensionName%" from "%installPath%" to "%destination%"...
 
-::Copy the files
-(COPY /y "%installPath%" "%destination%") || ECHO Oops! Fail to copy content from "%installPath%" to "%destination%"
-SET extensionInstalled=1
+:: Copy the files
+GOTO COPY_FILES
 
 
 :MSYSGIT
@@ -45,5 +48,47 @@ IF NOT EXIST "%exeInstall%" GOTO :INSTALLED_CHECK
 ECHO I'm installing "%gitExtensionName%" from "%installPath%" to "%destination%"...
 
 ::Copy the files
-(COPY /y "%installPath%" "%destination%") || ECHO Oops! Fail to copy content from "%installPath%" to "%destination%"
-SET extensionInstalled=1
+GOTO COPY_FILES
+
+:COPY_FILES
+(COPY /y "%installPath%"\*.exe "%destination%"\*.exe) || ECHO Oops! Fail to copy content from "%installPath%" to "%destination%"
+(COPY /y "%installPath%"\*.dll "%destination%"\*.dll) || ECHO Oops! Fail to copy content from "%installPath%" to "%destination%"
+SET helperInstalled=1
+
+:INSTALLED_CHECK
+
+::Check if Git was found or not
+IF %helperInstalled% == 1 GOTO :GIT_FOUND
+
+
+:NO_GIT_FOUND
+ECHO Git not found at the expected locations :(. Make sure Git is installed.
+GOTO :END
+
+
+:GIT_FOUND
+:: Pre-configure it for microsoft.visualstudio.com and mseng.visualstudio.com
+git config --global credential.helper !'%destination%\credential-helper-man.exe'
+git config --global credential.visualstudio.com.authority MSA
+git config --global credential.microsoft.visualstudio.com.authority MSA
+git config --global credential.mseng.visualstudio.com.authority MSA
+
+ECHO(
+ECHO Remote helper was installed!
+ECHO It was also configured for https://microsoft.visualstudio.com and https://mseng.visualstudio.com.
+ECHO(
+GOTO :END
+
+
+:LEGACY_OS
+ECHO Oops! 32-bit OS Not Supported
+GOTO :END
+
+
+:NEED_ADMIN_ACCESS
+ECHO You need to run this script elevated for it to work. Press ENTER to exit...
+pause >nul
+GOTO :END
+
+
+:END
