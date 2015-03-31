186e8df088c205588e6e1fd0da5c3e06ccc77084
J Wyman <jeremy.wyman@microsoft.com>
2015-03-31 10:51:16 -0700

    renamed AuthenticationTests to VsoMsaTests

---
+++ CoreTest/VsoMsaTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.TeamFoundation.Git.Helpers.Authentication
+{
+    [TestClass]
+    public class VsoMsaTests
+    {
+        [TestMethod]
+        public void VsoMsaInteractiveLogonTest()
+        {
+            VsoMsaAuthentation msa = new VsoMsaAuthentation();
+            msa.InteractiveLogon(new Uri("https://dev-x.visualstudio.com"));
+        }
+
+        [TestMethod]
+        public void VsoMsaRefreshCredentialsTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void VsoMsaSetCredentialsTest()
+        {
+
+        }
+    }
+}
