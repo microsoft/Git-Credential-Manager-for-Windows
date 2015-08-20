d9864905aebf1d1235d5a7cc3d2058eb5a3a3e2a
J Wyman <jeremy.wyman@microsoft.com>
2015-08-20 09:58:04 -0700

    Made `Global.GetUserAgent()` caching.

--- Microsoft.TeamFoundation.Authentication/Global.cs
+++ Microsoft.TeamFoundation.Authentication/Global.cs
@@ -4,19 +4,25 @@ namespace Microsoft.TeamFoundation.Authentication
 {
     internal static class Global
     {
+        private static string _useragent = null;
+
         /// <summary>
         /// Creates the correct user-agent string for HTTP calls.
         /// </summary>
         /// <returns>The `user-agent` string for "git-tools".</returns>
         public static string GetUserAgent()
         {
-            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-            return String.Format("git-credential-manager ({0}; {1}; {2}) CLR/{3} git-tools/{4}",
-                                 Environment.OSVersion.VersionString,
-                                 Environment.OSVersion.Platform,
-                                 Environment.Is64BitOperatingSystem ? "x64" : "x86",
-                                 Environment.Version.ToString(3),
-                                 version.ToString(3));
+            if (_useragent == null)
+            {
+                Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                _useragent = String.Format("git-credential-manager ({0}; {1}; {2}) CLR/{3} git-tools/{4}",
+                                           Environment.OSVersion.VersionString,
+                                           Environment.OSVersion.Platform,
+                                           Environment.Is64BitOperatingSystem ? "x64" : "x86",
+                                           Environment.Version.ToString(3),
+                                           version.ToString(3));
+            }
+            return _useragent;
         }
     }
 }
