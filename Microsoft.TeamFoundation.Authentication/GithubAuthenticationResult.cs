39d97c557a3b7fac83b5f4c4559b9b23ffabc20a
J Wyman <jeremy.wyman@microsoft.com>
2015-08-20 09:59:08 -0700

    Added a new complex return type for GitHub authentication.

---
+++ Microsoft.TeamFoundation.Authentication/GithubAuthenticationResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Microsoft.TeamFoundation.Authentication
+{
+    public struct GithubAuthenticationResult
+    {
+        public GithubAuthenticationResult(GithubAuthenticationResultType type)
+        {
+            Type = type;
+        }
+
+        public readonly GithubAuthenticationResultType Type;
+
+        public static implicit operator Boolean(GithubAuthenticationResult result)
+        {
+            return result.Type != GithubAuthenticationResultType.Failure;
+        }
+    }
+
+    public enum GithubAuthenticationResultType
+    {
+        Success,
+        Failure,
+        TwoFactorApp,
+        TwoFactorSms,
+    }
+}
