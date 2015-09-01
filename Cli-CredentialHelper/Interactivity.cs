aecfd114000c3f9978ea31d4aedd81bdb23c4a9b
Oli Dagenais <olivida@microsoft.com>
2015-08-31 23:25:53 -0400

    Fix typos in Interactivity enum.

--- Cli-CredentialHelper/Interactivity.cs
+++ Cli-CredentialHelper/Interactivity.cs
@@ -6,15 +6,15 @@
     internal enum Interactivity
     {
         /// <summary>
-        /// Present an interactive logon prompt when necissary, otherwise used cached credentials
+        /// Present an interactive logon prompt when necessary, otherwise use cached credentials
         /// </summary>
         Auto,
         /// <summary>
-        /// Always present an interactive logon prompt regardless of if cached credentials exist
+        /// Always present an interactive logon prompt regardless if cached credentials exist
         /// </summary>
         Always,
         /// <summary>
-        /// Never present an present an interactive logon prompt, fail without cached credentals
+        /// Never present an present an interactive logon prompt, fail without cached credentials
         /// </summary>
         Never
     }
