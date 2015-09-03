c5bdf02db3149a62e55e59715ff76488c3aabfad
Oli Dagenais <olivida@microsoft.com>
2015-09-03 13:26:34 -0400

    Add test to verify overwriting of values.

--- Test/ConfigurationTests.cs
+++ Test/ConfigurationTests.cs
@@ -24,6 +24,22 @@ namespace Microsoft.TeamFoundation.Authentication.Test
             Assert.AreEqual("false", values["core.autocrlf"]);
         }
 
+        [TestMethod]
+        public void ParseGitConfig_OverwritesValues()
+        {
+            // http://thedailywtf.com/articles/What_Is_Truth_0x3f_
+            const string input = @"
+[core]
+    autocrlf = true
+    autocrlf = FileNotFound
+    autocrlf = false
+";
+
+            var values = TestParseGitConfig(input);
+
+            Assert.AreEqual("false", values["core.autocrlf"]);
+        }
+
         private static Dictionary<string, string> TestParseGitConfig(string input)
         {
             var values = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
