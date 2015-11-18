100f39cc06761cb3d4838c6b63dd58b6c7fa2b75
Heath Stewart <heaths@microsoft.com>
2015-11-18 15:05:14 -0800

    Check for .NET 4.5.1 in a scalable manner
    Fixes issue #59 by using batch support for numeric comparisons.

--- Cli-CredentialHelper/install.cmd
+++ Cli-CredentialHelper/install.cmd
@@ -1,6 +1,7 @@
 :: Wanto to update this thing and don't know how?
 :: Check http://ss64.com/nt/syntax.html
 @ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
 
 :: globals
 SET netfx=0
@@ -17,23 +18,11 @@ IF "%~1" EQU "/?" (
 
     :: Detect if NETFX 4.5.1 or greater is installed
     :: https://msdn.microsoft.com/en-us/library/hh925568(v=vs.110).aspx
-    :: 0x5C733 - .NET Framework 4.5.1 installed with Windows 8.1 or Windows Server 2012 R2
-    :: 0x5C786 - .NET Framework 4.5.1 installed on Windows 8, Windows 7 SP1, or Windows Vista SP2
-    :: 0x5CBF5 - .NET Framework 4.5.2
-    :: 0x6004F - .NET Framework 4.6 installed with Windows 10
-    :: 0x60051 - .NET Framework 4.6 installed on all other OS version other than Windows 10
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client"| findstr "Release"| findstr /I "0x5C733" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client"| findstr "Release"| findstr /I "0x5C786" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client"| findstr "Release"| findstr /I "0x5CBF5" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client"| findstr "Release"| findstr /I "0x6004F" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client"| findstr "Release"| findstr /I "0x60051" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"| findstr "Release"| findstr /I "0x5C733" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"| findstr "Release"| findstr /I "0x5C786" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"| findstr "Release"| findstr /I "0x5CBF5" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"| findstr "Release"| findstr /I "0x6004F" 1>nul 2>&1) && SET netfx=1
-    (REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full"| findstr "Release"| findstr /I "0x60051" 1>nul 2>&1) && SET netfx=1
+    FOR /F "usebackq tokens=1,3" %%i in (`REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Client" /v Release`) DO IF "%%i" EQU "Release" SET /a netfx=%%j
+    FOR /F "usebackq tokens=1,3" %%i in (`REG QUERY "HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v Release`) DO IF "%%i" EQU "Release" SET /a netfx=%%j
 
-    IF %netfx% NEQ 1 (
+    :: 0x5C733 - .NET Framework 4.5.1 installed with Windows 8.1 or Windows Server 2012 R2
+    IF %netfx% LSS 378675 (
         GOTO :NETFX_NOT_FOUND
     )
     
