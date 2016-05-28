8592e9c93d5015310508590e197fd2c969c3dca1
Phil Haack <haacked@gmail.com>
2016-05-28 16:11:51 -0700

    Add a unit test for the MaskedPasswordBox

---
+++ GitHub.Authentication.Test/Controls/MaskedPasswordBoxTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHub.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHub.Authentication.Test.Controls
+{
+    [TestClass]
+    public class MaskedPasswordBoxTests
+    {
+        [TestMethod]
+        public void TextReplacedWithMaskCharacterAndPasswordPropertyContainsRealPassword()
+        {
+            var passwordTextInput = new MaskedPasswordBox();
+            // Set the base Text property. The one that entering text into the UI would set.
+            ((PromptTextBox)passwordTextInput).Text = "secr3t!";
+
+            Assert.AreEqual("●●●●●●●", ((PromptTextBox)passwordTextInput).Text);
+            Assert.AreEqual("secr3t!", passwordTextInput.Password);
+        }
+    }
+}
