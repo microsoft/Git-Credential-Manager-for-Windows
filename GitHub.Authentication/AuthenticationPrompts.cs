8e2c154591d1d5aec9f2508b0151774936edc6f4
Phil Haack <haacked@gmail.com>
2016-05-27 21:49:04 -0700

    Can only launch app once per AppDomain

--- GitHub.Authentication/AuthenticationPrompts.cs
+++ GitHub.Authentication/AuthenticationPrompts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,6 +17,8 @@ namespace GitHub.Authentication
             Trace.WriteLine("Program::GithubCredentialModalPrompt");
 
             var credentialViewModel = new CredentialsViewModel();
+
+            Trace.WriteLine("   prompting user for credentials.");
 
             bool credentialValid = ShowViewModel(credentialViewModel, () => new CredentialsWindow());
 
@@ -46,21 +49,15 @@ namespace GitHub.Authentication
         {
             StartSTATask(() =>
             {
-                if (!UriParser.IsKnownScheme("pack"))
-                {
-                    UriParser.Register(new GenericUriParser(GenericUriParserOptions.GenericAuthority), "pack", -1);
-                }
-                var app = new Application();
-                var appResources = new Uri("pack://application:,,,/GitHub.Authentication;component/AppResources.xaml", UriKind.RelativeOrAbsolute);
-                app.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = appResources });
+                EnsureApplicationResources();
                 var window = windowCreator();
                 window.DataContext = viewModel;
-                app.Run(window);
+                window.ShowDialog();
             })
             .Wait();
 
             return viewModel.Result == AuthenticationDialogResult.Ok
-                && viewModel.IsValid; ;
+                && viewModel.IsValid;
         }
 
         static Task StartSTATask(Action action)
@@ -81,6 +78,40 @@ namespace GitHub.Authentication
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
             return completionSource.Task;
+        }
+
+        static void EnsureApplicationResources()
+        {
+            if (!UriParser.IsKnownScheme("pack"))
+            {
+                UriParser.Register(new GenericUriParser(GenericUriParserOptions.GenericAuthority), "pack", -1);
+            }
+
+            var appResourcesUri = new Uri("pack://application:,,,/GitHub.Authentication;component/AppResources.xaml", UriKind.RelativeOrAbsolute);
+
+            // If we launch two dialogs in the same process (Credential followed by 2fa), calling new App() 
+            // throws an exception stating the Application class  can't be created twice. Creating an App
+            // instance happens to set Application.Current to that instance (it's weird). However, if you
+            // don't set the ShutdownMode to OnExplicitShutdown, the second time you launch a dialog, 
+            // Application.Current is null even in the same process.
+            if (Application.Current == null)
+            {
+                var app = new Application();
+                Debug.Assert(Application.Current == app, "Current application not set");
+                app.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+                app.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = appResourcesUri });
+            }
+            else
+            {
+                // Application.Current exists, but what if in the future, some other code created
+                // the singleton. Let's make sure our resources are still loaded.
+                var resourcesExist = Application.Current.Resources.MergedDictionaries.Any(r => r.Source == appResourcesUri);
+                if (!resourcesExist)
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = appResourcesUri });
+                }
+            }
+
         }
     }
 }
