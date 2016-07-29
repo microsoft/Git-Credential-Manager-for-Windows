14434f3e29fc5c599b7e00d0914cb39dd06113e2
J Wyman <jeremy.wyman@microsoft.com>
2016-07-29 10:37:39 -0400

    Allow for setting null proxy

--- Cli-CredentialHelper/OperationArguments.cs
+++ Cli-CredentialHelper/OperationArguments.cs
@@ -224,11 +224,12 @@ namespace Microsoft.Alm.CredentialHelper
 
         public void SetProxy(string url)
         {
-            Uri tmp;
+            Uri tmp = null;
             if (Uri.TryCreate(url, UriKind.Absolute, out tmp))
             {
-                this.ProxyUri = tmp;
+                Trace.WriteLine("   successfully set proxy to " + tmp.AbsoluteUri);
             }
+            this.ProxyUri = tmp;
         }
 
         public override string ToString()
