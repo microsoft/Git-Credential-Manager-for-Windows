d5b58ebdac8b03b559b3b247db28492e2b2c8bb6
XhmikosR <xhmikosr@gmail.com>
2016-12-26 11:05:22 +0200

    Tweak docs generation.
    * Use html5 doctype
    * Include only the basic Bootstrap styles (saves ~200KB per file)

--- Cli-CredentialHelper/build-docs.cmd
+++ Cli-CredentialHelper/build-docs.cmd
@@ -12,8 +12,8 @@ IF NOT EXIST "%dstdir%" (
 )
 
 IF EXIST %srcdir% (
-    (WHERE pandoc > nul 1>&2) && (pandoc --from=markdown --include-in-header="%assets%\help.css" --to=html --normalize --output="%dstdir%\git-credential-manager.html" "%srcdir%\CredentialManager.md" "%srcdir%\Configuration.md" "%srcdir%\Faq.md" "%srcdir%\Automation.md" "%srcdir%\Development.md" > nul 1>&2) && (ECHO git-credential-manager.html generated.)
-    (WHERE pandoc > nul 1>&2) && (pandoc --from=markdown --include-in-header="%assets%\help.css" --to=html --normalize --output="%dstdir%\git-askpass.html" "%srcdir%\Askpass.md" "%srcdir%\Configuration.md" "%srcdir%\Faq.md" "%srcdir%\Automation.md" "%srcdir%\Development.md" > nul 1>&2) && (ECHO git-askpass.html generated.)
+    (WHERE pandoc > nul 1>&2) && (pandoc --from=markdown --to=html5 --self-contained --standalone --include-in-header="%assets%\head_css.html" --normalize --output="%dstdir%\git-credential-manager.html" "%srcdir%\CredentialManager.md" "%srcdir%\Configuration.md" "%srcdir%\Faq.md" "%srcdir%\Automation.md" "%srcdir%\Development.md" > nul 1>&2) && (ECHO git-credential-manager.html generated.)
+    (WHERE pandoc > nul 1>&2) && (pandoc --from=markdown --to=html5 --self-contained --standalone --include-in-header="%assets%\head_css.html" --normalize --output="%dstdir%\git-askpass.html" "%srcdir%\Askpass.md" "%srcdir%\Configuration.md" "%srcdir%\Faq.md" "%srcdir%\Automation.md" "%srcdir%\Development.md" > nul 1>&2) && (ECHO git-askpass.html generated.)
 )
 
 EXIT /B 0
