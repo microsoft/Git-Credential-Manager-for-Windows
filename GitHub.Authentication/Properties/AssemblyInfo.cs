cd231cc78bd931d71f8d289ffb7fe04bb2e2cccd
J Wyman <jeremy.wyman@microsoft.com>
2017-01-04 12:16:42 -0500

    Update version numbers and copyright years.
    Update all copyright years from 2016 to 2017, just a janitorial thing.
    
    Update version for askpass and credential-manager to 1.9.0, MS.Alm.Authentication to 3.3.0, and GH.Authentication to 1.1.1 .

--- GitHub.Authentication/Properties/AssemblyInfo.cs
+++ GitHub.Authentication/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("GitHub")]
 [assembly: AssemblyProduct("GitHub.Authentication")]
-[assembly: AssemblyCopyright("Copyright © GitHub 2016")]
+[assembly: AssemblyCopyright("Copyright © GitHub 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,5 +47,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
