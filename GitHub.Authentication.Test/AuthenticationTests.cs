0bd33e23f42651f2fa9dd4ca9c271651cc5d8e6b
Phil Haack <haacked@gmail.com>
2017-09-29 11:38:20 -0700

    Add some tests for URL normalization

---
+++ GitHub.Authentication.Test/AuthenticationTests.cs
@@ -0,0 +1,51 @@
+﻿using GitHub.Authentication.Test.Fakes;
+using Microsoft.Alm.Authentication;
+using System;
+using Xunit;
+
+namespace GitHub.Authentication.Test
+{
+    public class AuthenticationTests
+    {
+        [Theory]
+        [InlineData("https://github.com/", "https://github.com/")]
+        [InlineData("https://gist.github.com/", "https://gist.github.com/")]
+        [InlineData("https://github.com/", "https://gist.github.com/")]
+        [InlineData("https://gist.github.com/", "https://gist.github.com/")]
+        public void GetSetCredentialsNormalizesGistUrls(string writeUriString, string retrieveUriString)
+        {
+            var retrieveUri = new Uri(retrieveUriString);
+            var credentialStore = new InMemoryCredentialStore();
+            
+            var authentication = new Authentication(
+                new Uri(writeUriString),
+                TokenScope.Gist,
+                credentialStore,
+                new Authentication.AcquireCredentialsDelegate(AuthenticationPrompts.CredentialModalPrompt),
+                new Authentication.AcquireAuthenticationCodeDelegate(AuthenticationPrompts.AuthenticationCodeModalPrompt),
+                null);
+
+            authentication.SetCredentials(new Uri(writeUriString), new Credential("haacked"));
+            Assert.Equal("haacked", authentication.GetCredentials(retrieveUri).Username);
+        }
+
+        [Fact]
+        public void GetSetCredentialsDoesNotReturnCredentialForRandomUrl()
+        {
+            var retrieveUri = new Uri("https://example.com/");
+            var credentialStore = new InMemoryCredentialStore();
+
+            var authentication = new Authentication(
+                new Uri("https://github.com/"),
+                TokenScope.Gist,
+                credentialStore,
+                new Authentication.AcquireCredentialsDelegate(AuthenticationPrompts.CredentialModalPrompt),
+                new Authentication.AcquireAuthenticationCodeDelegate(AuthenticationPrompts.AuthenticationCodeModalPrompt),
+                null);
+
+            authentication.SetCredentials(new Uri("https://github.com/"), new Credential("haacked"));
+
+            Assert.Null(authentication.GetCredentials(retrieveUri));
+        }
+    }
+}
