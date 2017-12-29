1acb31a2a748a40f9e6eb74e15c648c075a58dba
ebugusey <ebugusey@users.noreply.github.com>
2017-12-29 16:09:02 +0400

    (Cli-Askpass) Fix ArgumentNullException when username is not set in prompt URL
    Arguments `username` and `password` shouldn't be null, when you call
    `operationArguments.SetCredentials()`.

--- Cli-Askpass/Program.cs
+++ Cli-Askpass/Program.cs
@@ -208,7 +208,7 @@ namespace Microsoft.Alm.Cli
                         Git.Trace.WriteLine($"success parsing URL, targetUri = '{targetUri}'.");
 
                         OperationArguments operationArguments = new OperationArguments.Impl(targetUri);
-                        operationArguments.SetCredentials(username, password);
+                        operationArguments.SetCredentials(username ?? string.Empty, password ?? string.Empty);
 
                         // load up the operation arguments, enable tracing, and query for credentials
                         LoadOperationArguments(operationArguments);
