82496fbb01f709869757d9355238ca42773be7d1
J Wyman ∞ <jeremy.wyman@outlook.com>
2018-03-06 15:31:45 -0500

    authentication: Convert `Where` to a standard class.
    Convert `Where` from a static singleton into a standard, instatiatable class type with an interface. Change the accibility of `Where` from `public` to `internal`, and the new interface assumes the `public` API.

--- Microsoft.Alm.Authentication/Git/Where.cs
+++ Microsoft.Alm.Authentication/Git/Where.cs
@@ -32,16 +32,129 @@ using Microsoft.Win32;
 
 namespace Microsoft.Alm.Authentication.Git
 {
-    public static class Where
+    public interface IWhere
     {
         /// <summary>
         /// Finds the "best" path to an app of a given name.
         /// <para/>
         /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
         /// </summary>
-        /// <param name="name">The name of the application, without extension, to find.</param>
-        /// <param name="path">Path to the first match file which the operating system considers executable.</param>
-        static public bool FindApp(string name, out string path)
+        /// <param name="name">
+        /// The name of the application, without extension, to find.
+        /// </param>
+        /// <param name="path">
+        /// Path to the first match file which the operating system considers executable.
+        /// </param>
+        bool FindApp(string name, out string path);
+
+        /// <summary>
+        /// Finds and returns path(s) to Git installation(s) in common locations.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="path">
+        /// The local respository path if any.
+        /// <para/>
+        /// Used to find local Git configuration files.
+        /// </param>
+        /// <param name="distro">
+        /// The distribution/version of Git used.
+        /// <para/>
+        /// Used to find system Git configuration files.
+        /// </param>
+        /// <param name="installations">
+        /// The list of found Git installation if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool FindGitInstallation(string path, KnownDistribution distro, out Installation installation);
+
+        /// <summary>
+        /// Finds and returns path(s) to Git installation(s) in common locations.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="installations">The list of found Git installation if successful; otherwise `<see langword="null"/>`.</param>
+        bool FindGitInstallations(out List<Installation> installations);
+
+        /// <summary>
+        /// Gets the path to Git's global configuration file.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="path">
+        /// Path to Git's global configuration if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool GitGlobalConfig(out string path);
+
+        /// <summary>
+        /// Gets the path to Git's local configuration file based on the current working directory.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="path">
+        /// Path to Git's local configuration if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool GitLocalConfig(out string path);
+
+        /// <summary>
+        /// Gets the path to Git's portable system configuration file.
+        /// <para/>
+        /// Searches starting with `<paramref name="startingDirectory"/>` working up towards the device root.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="startingDirectory">
+        /// Working directory of the repository to find the configuration.
+        /// </param>
+        /// <param name="path">
+        /// Path to Git's local configuration if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool GitLocalConfig(string startingDirectory, out string path);
+
+        /// <summary>
+        /// Gets the path to Git's portable system configuration file.
+        /// <para/>
+        /// Searches starting with `<see cref="Environment.CurrentDirectory"/>` working up towards the device root.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="path">
+        /// Path to Git's portable system configuration if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool GitPortableConfig(out string path);
+
+        /// <summary>
+        /// Gets the path to Git's system configuration file.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="path">
+        /// Path to Git's system configuration if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool GitSystemConfig(Installation? installation, out string path);
+
+        /// <summary>
+        /// Gets the path to Git's XDG configuration file.
+        /// <para/>
+        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
+        /// </summary>
+        /// <param name="path">
+        /// Path to Git's XDG configuration file if successful; otherwise `<see langword="null"/>`.
+        /// </param>
+        bool GitXdgConfig(out string path);
+
+        /// <summary>
+        /// Returns the path to the user's home directory (~/ or %HOME%) that Git will rely on.
+        /// </summary>
+        string Home();
+    }
+
+    internal class Where : Base, IWhere
+    {
+        public Where(RuntimeContext context)
+            : base(context)
+        { }
+
+        public bool FindApp(string name, out string path)
         {
             if (!string.IsNullOrWhiteSpace(name))
             {
@@ -84,19 +197,13 @@ namespace Microsoft.Alm.Authentication.Git
             return false;
         }
 
-        public static bool FindGitInstallation(string path, KnownDistribution distro, out Installation installation)
+        public bool FindGitInstallation(string path, KnownDistribution distro, out Installation installation)
         {
             installation = new Installation(path, distro);
             return Installation.IsValid(installation);
         }
 
-        /// <summary>
-        /// Finds and returns path(s) to Git installation(s) in common locations.
-        /// <para/>
-        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
-        /// </summary>
-        /// <param name="installations">The list of found Git installation if successful; otherwise `<see langword="null"/>`.</param>
-        public static bool FindGitInstallations(out List<Installation> installations)
+        public bool FindGitInstallations(out List<Installation> installations)
         {
             const string GitAppName = @"Git";
             const string GitSubkeyName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Git_is1";
@@ -163,7 +270,7 @@ namespace Microsoft.Alm.Authentication.Git
 
             List<Installation> candidates = new List<Installation>();
             // Add candidate locations in order of preference.
-            if (Where.FindApp(GitAppName, out shellPathValue))
+            if (FindApp(GitAppName, out shellPathValue))
             {
                 // `Where.App` returns the path to the executable, truncate to the installation root
                 if (shellPathValue.EndsWith(Installation.AllVersionCmdPath, StringComparison.OrdinalIgnoreCase))
@@ -233,15 +340,12 @@ namespace Microsoft.Alm.Authentication.Git
 
             installations = pathSet.ToList();
 
-            // Git.Trace.WriteLine($"found {installations.Count} Git installation(s).");
+            Trace.WriteLine($"found {installations.Count} Git installation(s).");
 
             return installations.Count > 0;
         }
 
-        /// <summary>
-        /// Returns the path to the user's home directory (~/ or %HOME%) that Git will rely on.
-        /// </summary>
-        public static string Home()
+        public string Home()
         {
             // Git relies on the %HOME% environment variable to represent the users home directory it
             // can contain embedded environment variables, so we need to expand it
@@ -273,13 +377,7 @@ namespace Microsoft.Alm.Authentication.Git
             return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         }
 
-        /// <summary>
-        /// Gets the path to Git's global configuration file.
-        /// <para/>
-        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
-        /// </summary>
-        /// <param name="path">Path to Git's global configuration if successful; otherwise `<see langword="null"/>`.</param>
-        public static bool GitGlobalConfig(out string path)
+        public bool GitGlobalConfig(out string path)
         {
             const string GlobalConfigFileName = ".gitconfig";
 
@@ -299,18 +397,9 @@ namespace Microsoft.Alm.Authentication.Git
             return false;
         }
 
-        /// <summary>
-        /// Gets the path to Git's portable system configuration file.
-        /// <para/>
-        /// Searches starting with `<paramref name="startingDirectory"/>` working up towards the device root.
-        /// <para/>
-        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
-        /// </summary>
-        /// <param name="startingDirectory">Working directory of the repository to find the configuration.</param>
-        /// <param name="path">Path to Git's local configuration if successful; otherwise `<see langword="null"/>`.</param>
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times")]
-        public static bool GitLocalConfig(string startingDirectory, out string path)
+        public bool GitLocalConfig(string startingDirectory, out string path)
         {
             const string GitFolderName = ".git";
             const string LocalConfigFileName = "config";
@@ -412,25 +501,12 @@ namespace Microsoft.Alm.Authentication.Git
             return false;
         }
 
-        /// <summary>
-        /// Gets the path to Git's local configuration file based on the current working directory.
-        /// </summary>
-        /// <param name="path">Path to Git's local configuration if successful; otherwise `<see langword="null"/>`.</param>
-        /// <returns><see langword="True"/> if succeeds; <see langword="false"/> otherwise.</returns>
-        public static bool GitLocalConfig(out string path)
+        public bool GitLocalConfig(out string path)
         {
             return GitLocalConfig(Environment.CurrentDirectory, out path);
         }
 
-        /// <summary>
-        /// Gets the path to Git's portable system configuration file.
-        /// <para/>
-        /// Searches starting with `<see cref="Environment.CurrentDirectory"/>` working up towards the device root.
-        /// <para/>
-        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
-        /// </summary>
-        /// <param name="path">Path to Git's portable system configuration if successful; otherwise `<see langword="null"/>`.</param>
-        public static bool GitPortableConfig(out string path)
+        public bool GitPortableConfig(out string path)
         {
             const string PortableConfigFolder = "Git";
             const string PortableConfigFileName = "config";
@@ -447,13 +523,7 @@ namespace Microsoft.Alm.Authentication.Git
             return path != null;
         }
 
-        /// <summary>
-        /// Gets the path to Git's system configuration file.
-        /// <para/>
-        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
-        /// </summary>
-        /// <param name="path">Path to Git's system configuration if successful; otherwise `<see langword="null"/>`.</param>
-        public static bool GitSystemConfig(Installation? installation, out string path)
+        public bool GitSystemConfig(Installation? installation, out string path)
         {
             if (installation.HasValue && File.Exists(installation.Value.Config))
             {
@@ -477,13 +547,7 @@ namespace Microsoft.Alm.Authentication.Git
             return false;
         }
 
-        /// <summary>
-        /// Gets the path to Git's XDG configuration file.
-        /// <para/>
-        /// Returns `<see langword="true"/>` if successful; otherwise `<see langword="false"/>`.
-        /// </summary>
-        /// <param name="path">Path to Git's XDG configuration file if successful; otherwise `<see langword="null"/>`.</param>
-        public static bool GitXdgConfig(out string path)
+        public bool GitXdgConfig(out string path)
         {
             const string XdgConfigFolder = "Git";
             const string XdgConfigFileName = "config";
