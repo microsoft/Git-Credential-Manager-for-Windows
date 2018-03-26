cc631226cb20a8c32e3417777b897eca8ac15833
Kim Gybels <kgybels@infogroep.be>
2018-03-27 00:11:17 +0200

    Remove special character from test EmailAsUserName

--- Cli-CredentialHelper.Test/OperationArgumentsTests.cs
+++ Cli-CredentialHelper.Test/OperationArgumentsTests.cs
@@ -91,7 +91,7 @@ namespace Microsoft.Alm.Cli.Test
             var input = new InputArg
             {
                 Host = "example.visualstudio.com",
-                Password = "ḭncorrect",
+                Password = "incorrect",
                 Path = "path",
                 Protocol = Uri.UriSchemeHttps,
                 Username = "userName@domain.com"
