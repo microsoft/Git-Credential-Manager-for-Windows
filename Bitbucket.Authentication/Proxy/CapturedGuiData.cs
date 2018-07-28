fe72360d09a726064ce3b737e318fb0d9b757180
J Wyman <jeremy.wyman@outlook.com>
2018-07-27 23:58:33 -0400

    bitbucket: add capture and replay proxy.
    Provide capture and reply proxy project for 'Atlassian.Bitbucket.Authentication'.
    
     - [x] Provide capture proxy.
     - [x] Provide replay proxy.
     - [x] Extend `Microsoft.Alm.Authentication.Test.UnitTestBase` with specialized support for Bitbucket via `IGui`.

---
+++ Bitbucket.Authentication/Proxy/CapturedGuiData.cs
@@ -0,0 +1,101 @@
+﻿/**** Git Credential Manager for Windows ****
+ *
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the """"Software""""), to deal
+ * in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+**/
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using static System.FormattableString;
+
+namespace Atlassian.Bitbucket.Authentication.Test
+{
+    [System.Diagnostics.DebuggerDisplay("{DebuggerDisplay, nq}")]
+    public struct CapturedGuiData
+    {
+        [JsonProperty(PropertyName = "Operations", NullValueHandling = NullValueHandling.Ignore)]
+        public List<CapturedGuiOperation> Operations { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get { return Invariant($"{nameof(CapturedGuiData)}: {nameof(Operations)}[{Operations?.Count}]"); }
+        }
+
+        public static bool TryDeserialize(object serializedData, out CapturedGuiData guiData)
+        {
+            if (serializedData is JObject jGuiData)
+            {
+                guiData = jGuiData.ToObject<CapturedGuiData>();
+
+                return true;
+            }
+
+            guiData = default(CapturedGuiData);
+            return false;
+        }
+    }
+
+    [System.Diagnostics.DebuggerDisplay("{DebuggerDisplay, nq}")]
+    public struct CapturedGuiOperation
+    {
+        [JsonProperty(PropertyName = "Output", NullValueHandling = NullValueHandling.Ignore)]
+        public CapturedGuiOutput Output { get; set; }
+
+        [JsonProperty(PropertyName = "DialogType", NullValueHandling = NullValueHandling.Ignore)]
+        public string DialogType { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get { return Invariant($"{nameof(CapturedGuiOperation)}: {nameof(DialogType)} = {DialogType}"); }
+        }
+    }
+
+    [System.Diagnostics.DebuggerDisplay("{DebuggerDisplay, nq}")]
+    public struct CapturedGuiOutput
+    {
+        [JsonProperty(PropertyName = "Login", NullValueHandling = NullValueHandling.Ignore)]
+        public string Login { get; set; }
+
+        [JsonProperty(PropertyName = "IsValid", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsValid { get; set; }
+
+        [JsonProperty(PropertyName = "Password", NullValueHandling = NullValueHandling.Ignore)]
+        public string Password { get; set; }
+
+        [JsonProperty(PropertyName = "Result", NullValueHandling = NullValueHandling.Ignore)]
+        public int Result { get; set; }
+
+        [JsonProperty(PropertyName = "Success", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Success { get; set; }
+
+        internal string DebuggerDisplay
+        {
+            get { return Invariant($"{nameof(CapturedGuiOutput)}: {ToString()}"); }
+        }
+
+        public override string ToString()
+        {
+            return Invariant($"{nameof(Result)} = {Result}, {nameof(Success)} = {Success}");
+        }
+    }
+}
---
+++ Bitbucket.Authentication/Proxy/UnitTestBase.cs
@@ -0,0 +1,73 @@
+﻿/**** Git Credential Manager for Windows ****
+ *
+ * Copyright (c) Microsoft Corporation
+ * All rights reserved.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the """"Software""""), to deal
+ * in the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
+**/
+
+using System.Runtime.CompilerServices;
+using Microsoft.Alm.Authentication.Test;
+
+namespace Atlassian.Bitbucket.Authentication.Test
+{
+    public class UnitTestBase : Microsoft.Alm.Authentication.Test.UnitTestBase
+    {
+        protected UnitTestBase(IUnitTestTrace output, string projectDirectory, [CallerFilePath] string filePath = "")
+            : base(output, projectDirectory, filePath)
+        {
+            if (GetService<IGui>() is null)
+            {
+                SetService(new Gui(Context));
+            }
+        }
+
+        protected UnitTestBase(IUnitTestTrace output, [CallerFilePath] string filePath = "")
+            : this(output, null, filePath)
+        { }
+
+        protected override void InitializeTest(int iteration = -1, [CallerMemberName] string testName = "")
+        {
+            switch (TestMode)
+            {
+                case UnitTestMode.Capture:
+                {
+                    var serviceGui = GetService<IGui>();
+                    var captureGui = new CaptureGui(Context, serviceGui);
+
+                    SetService(captureGui);
+                }
+                break;
+
+                case UnitTestMode.NoProxy: break;
+
+                case UnitTestMode.Replay:
+                {
+                    var replayGui = new ReplayGui(Context);
+
+                    SetService(replayGui);
+                }
+                break;
+            }
+
+            base.InitializeTest(iteration, testName);
+        }
+    }
+}
